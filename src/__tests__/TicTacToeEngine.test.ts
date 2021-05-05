7b3911037e3f768fa6e99901717cc6e1108cc932
davidcwh <dcwh12321@gmail.com>
2021-05-05 16:46:58 +0800

    Added getBestMove tests

--- src/__tests__/TicTacToeEngine.test.ts
+++ src/__tests__/TicTacToeEngine.test.ts
@@ -323,3 +323,162 @@ describe("Tic Tac Toe Engine Make Next Move test suite", () => {
         }).toThrow(new TerminatedGameException('Player 1 Unable to make move at cell (y=0, x=0) - Game has already terminated'));
     });
 });
+
+
+describe("Tic Tac Toe Engine Get Best Move test suite", () => {
+    test("Get best move for Player Two simple test - 3 options", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_TWO);
+        /*
+        1 |   | 2
+          | 2 |  
+        1 | 1 | 2
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 1)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(2, 0)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player one move
+
+        const expected = {x: 2, y: 1};
+        const actual = ticTacToeEngine.getBestMove();
+        expect(actual).toEqual(expected);
+    });
+
+    test("Get best move for Player One simple test - 3 options", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_ONE);
+        /*
+        2 |   | 1
+          | 1 |  
+        2 | 2 | 1
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 1)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(2, 0)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player one move
+
+        const expected = {x: 2, y: 1};
+        const actual = ticTacToeEngine.getBestMove();
+        expect(actual).toEqual(expected);
+    });
+
+    test("Get best move for Player One depth case", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_ONE);
+        /*
+          | 1 |  
+        2 |   | 1
+        2 | 2 | 1
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 1)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(2, 1)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player two move
+
+        const expected = {x: 2, y: 0};
+        const actual = ticTacToeEngine.getBestMove();
+        expect(actual).toEqual(expected);
+    });
+
+    test("Get best move for Player Two depth case", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_TWO);
+        /*
+          | 2 |  
+        1 |   | 2
+        1 | 1 | 2
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 1)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(2, 1)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player two move
+
+        const expected = {x: 2, y: 0};
+        const actual = ticTacToeEngine.getBestMove();
+        expect(actual).toEqual(expected);
+    });
+
+    test("Get best move for Player One multiple moves case", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_ONE);
+        /*
+        1 | 1 |  
+        2 |   |  
+        2 | 2 | 1
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 1)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 0)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player two move
+
+        const expected = [{x: 1, y: 1}, {x: 2, y: 0}];
+        const actual = ticTacToeEngine.getBestMove();
+        expect(expected).toContainEqual(actual);
+    });
+
+    test("Get best move for Player Two multiple moves case", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_TWO);
+        /*
+        2 | 2 |  
+        1 |   |  
+        1 | 1 | 2
+        */
+        expect(ticTacToeEngine.makeNextMove(1, 0)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 1)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(0, 0)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(0, 2)).toEqual(GameStatus.ONGOING); // player one move
+        expect(ticTacToeEngine.makeNextMove(2, 2)).toEqual(GameStatus.ONGOING); // player two move
+        expect(ticTacToeEngine.makeNextMove(1, 2)).toEqual(GameStatus.ONGOING); // player one move
+
+        const expected = [{x: 1, y: 1}, {x: 2, y: 0}];
+        const actual = ticTacToeEngine.getBestMove();
+        expect(expected).toContainEqual(actual);
+    });
+
+    test("Get best move AI against AI", () => {
+        const ticTacToeEngine = new TicTacToeEngine(Player.PLAYER_ONE);
+        // If minimax algorithm works, both players making the best move everytime will always end in a draw
+        
+        const p1move1 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p1move1.x, p1move1.y)).toEqual(GameStatus.ONGOING); // player one move
+        const p2move1 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p2move1.x, p2move1.y)).toEqual(GameStatus.ONGOING); // player two move
+
+        const p1move2 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p1move2.x, p1move2.y)).toEqual(GameStatus.ONGOING); // player one move
+        const p2move2 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p2move2.x, p2move2.y)).toEqual(GameStatus.ONGOING); // player two move
+
+        const p1move3 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p1move3.x, p1move3.y)).toEqual(GameStatus.ONGOING); // player one move
+        const p2move3 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p2move3.x, p2move3.y)).toEqual(GameStatus.ONGOING); // player two move
+
+        const p1move4 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p1move4.x, p1move4.y)).toEqual(GameStatus.ONGOING); // player one move
+        const p2move4 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p2move4.x, p2move4.y)).toEqual(GameStatus.ONGOING); // player two move
+
+        const p1move5 = ticTacToeEngine.getBestMove();
+        expect(ticTacToeEngine.makeNextMove(p1move5.x, p1move5.y)).toEqual(GameStatus.DRAW); // player one move
+
+        // console.table(p1move1);
+        // console.table(p2move1);
+
+        // console.table(p1move2);
+        // console.table(p2move2);
+
+        // console.table(p1move3);
+        // console.table(p2move3);
+
+        // console.table(p1move4);
+        // console.table(p2move4);
+
+        // console.table(p1move5);
+    });
+})
